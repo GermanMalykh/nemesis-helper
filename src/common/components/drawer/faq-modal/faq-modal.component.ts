4afa19ce619d81bd2e208d54616c342537029cb6
German Malykh <108834841+GermanMalykh@users.noreply.github.com>
2025-05-06 18:50:17 +0300

    Update faq-modal.component.ts

--- src/common/components/drawer/faq-modal/faq-modal.component.ts
+++ src/common/components/drawer/faq-modal/faq-modal.component.ts
@@ -3,7 +3,11 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslationGroup } from '@common/interfaces/translation-group.interface';
 import { GameIdTkPipe } from '@common/pipes/game-id-tk/game-id-tk.pipe';
 import { GameKey } from '@configs/games.config';
-import { TranslateModule } from '@ngx-translate/core';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import { signal, WritableSignal, computed, effect } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 
 export interface FaqModalData {
     gameId: GameKey;
@@ -16,6 +20,11 @@ export interface FaqModalData {
     imports: [
         TranslateModule,
         GameIdTkPipe,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormField,
+        MatInput,
+        MatLabel,
     ],
     templateUrl: './faq-modal.component.html',
     styleUrl: './faq-modal.component.scss',
@@ -23,8 +32,27 @@ export interface FaqModalData {
 })
 export class FaqModalComponent {
     protected readonly data: FaqModalData = inject(MAT_DIALOG_DATA);
+    protected searchControl: FormControl<string> = new FormControl('', { nonNullable: true });
+    protected readonly originalData: TranslationGroup[] = this.data.faqGroups;
+    protected readonly filteredGroups: WritableSignal<TranslationGroup[]> = signal(this.originalData);
+    private readonly translate = inject(TranslateService);
 
     protected scrollInto(index: number): void {
         document.querySelector(`#group${index}`)?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+    public constructor() {
+        this.searchControl.valueChanges.pipe(debounceTime(200)).subscribe(search => {
+            const query = search.toLowerCase().trim();
+            const result = this.originalData
+                .map(group => ({
+                    ...group,
+                    items: group.items.filter(item => {
+                        const translated = this.translate.instant(item);
+                        return query === '' || translated.toLowerCase().includes(query);
+                    })
+                }))
+                .filter(group => group.items.length > 0);
+            this.filteredGroups.set(result);
+        });
     }
 }
